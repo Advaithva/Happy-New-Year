19ccca3da77d649aaeb90adf8ae876af72605cec
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2023-01-02 17:47:00 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function CLICK() {
+
+    document.getElementById('hny').innerHTML= 'Happy New Year 2023';
+    document.getElementById("p").innerHTML = 'Happy New Year!!! Let us welcome the year 2023 with happiness and enthusiasm. Wave goodbye to the old and embrace the new with hope, dreams, and ambition. Wishing you a Happy New Year full of happiness!';
+    document.getElementById("img").src = 'https://helonational.com/wp-content/uploads/2020/11/Happy-New-Year-Gif-8.gif';
+    document.getElementById("img").height = '250';
+    document.getElementById("img").width = '450';
+}
+
+
+function DTE() {
+    document.getElementById('time').innerHTML = Date();
+}
